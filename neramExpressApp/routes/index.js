d789d59db291ca95e380eceb616d2e728c1df5cb
Your Name <you@example.com>
2025-01-02 09:49:19 +0000

    onerror update

--- neramExpressApp/routes/index.js
+++ neramExpressApp/routes/index.js
@@ -14,13 +14,5 @@ router.get('/careers', function (req, res, next) {
   res.render('careers'); // Render the careers.ejs page
 });
 
-router.get('/debug-env', (req, res) => {
-  res.json({
-    storageAccountName: process.env.AZURE_STORAGE_ACCOUNT_NAME,
-    storageAccountKey: process.env.AZURE_STORAGE_ACCOUNT_KEY,
-    blobContainerName: process.env.AZURE_BLOB_CONTAINER_NAME,
-  });
-});
-
 
 module.exports = router;
